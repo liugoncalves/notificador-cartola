9c9c380c782eab3f35b72286878218af105c6c80
liugoncalves <110125502+liugoncalves@users.noreply.github.com>
2025-04-14 13:15:44 -0300

    atualização na exibição das horas e possibilidade de escolher a primeira partida da lista

--- scrap-brasileirao.py
+++ scrap-brasileirao.py
@@ -18,7 +18,7 @@ def send_whatsapp_message(message):
     )
     print(f"Mensagem enviada: {message.sid}")
 
-def get_proxima_partida():
+def get_proxima_partida(indice):
     url = "https://ge.globo.com/futebol/brasileirao-serie-a/"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
@@ -36,25 +36,34 @@ def get_proxima_partida():
 
     try:
         jogos_json = json.loads(match.group(1))
+        partidas_futuras = []
+
         for jogo in jogos_json:
             data_str = jogo.get("data_realizacao")
             if data_str:
                 data_jogo = datetime.strptime(data_str, "%Y-%m-%dT%H:%M")
                 if data_jogo > datetime.now():
-                    return {
+                    partidas_futuras.append({
                         "data": data_jogo,
                         "mandante": jogo["equipes"]["mandante"]["nome_popular"],
                         "visitante": jogo["equipes"]["visitante"]["nome_popular"],
                         "estadio": jogo["sede"]["nome_popular"]
-                    }
+                    })
+                
+        if len(partidas_futuras) > indice:
+            return partidas_futuras[indice]
+        else:
+            print(f"Não há partida no índice {indice}.")
+            return None
+                
     except Exception as e:
         print(f"Erro ao processar JSON: {e}")
         return None
 
     return None
 
-def check_jogo():
-    partida = get_proxima_partida()
+def check_jogo(indice):
+    partida = get_proxima_partida(indice)
     if not partida:
         print("Nenhuma partida futura encontrada.")
         return
@@ -62,36 +71,25 @@ def check_jogo():
     agora = datetime.now()
     diff = partida["data"] - agora
 
-    dias_faltando = diff.days
-    horas_restantes = int(diff.seconds // 3600)
-    minutos_restantes = int(diff.seconds // 60)
+    horas_restantes = int(diff.total_seconds() // 3600)
+    minutos_restantes = int((diff.total_seconds() % 3600) // 60)
 
     data_formatada = partida["data"].strftime("%d/%m/%Y às %H:%M")
 
-    if(dias_faltando > 0):
+    if(horas_restantes >= 1):
         mensagem = (
             f"⚠️ ATENÇÃO ⚠️\n\n"
-            f"🏆 Brasileirão - Rodada 3\n\n"
+            f"🏆 Brasileirão - Rodada 4\n\n"
             f"📅 Primeira partida: *{partida['mandante']} 🆚 {partida['visitante']}*\n"
             f"⏰ Horário: {data_formatada}\n"
             f"🏟️ Estádio: {partida['estadio']}\n\n"
-            f"⌛ Faltam *{dias_faltando} dias* e *{horas_restantes}* horas para o fechamento do Mercado do Cartola!"
-        )
-
-    elif(dias_faltando == 0 ):
-        mensagem = (
-            f"⚠️ ATENÇÃO ⚠️\n\n"
-            f"🏆 Brasileirão - Rodada 3\n\n"
-            f"📅 Primeira partida: *{partida['mandante']} 🆚 {partida['visitante']}*\n"
-            f"⏰ Horário: {data_formatada}\n"
-            f"🏟️ Estádio: {partida['estadio']}\n\n"
-            f"⌛ Faltam *{horas_restantes} horas* para o fechamento do Mercado do Cartola!"
+            f"⌛ Faltam *{horas_restantes} horas* e *{minutos_restantes} minutos* para o fechamento do Mercado do Cartola!"
         )
     
     elif(horas_restantes == 0 ):
         mensagem = (
             f"⚠️ ATENÇÃO ⚠️\n\n"
-            f"🏆 Brasileirão - Rodada 3\n\n"
+            f"🏆 Brasileirão - Rodada 4\n\n"
             f"📅 Primeira partida: *{partida['mandante']} 🆚 {partida['visitante']}*\n"
             f"⏰ Horário: {data_formatada}\n"
             f"🏟️ Estádio: {partida['estadio']}\n\n"
@@ -101,4 +99,4 @@ def check_jogo():
     send_whatsapp_message(mensagem)
 
 # Executa ao rodar
-check_jogo()
+check_jogo(1)
